1b54bd408eda4c054bb6ff960a8db9d4974159fc
Jonathan Louis <49454087+Jonathan-Louis@users.noreply.github.com>
2021-01-25 11:11:08 -0500

    Add files via upload
    initial commit

---
+++ views/projects.php
@@ -0,0 +1,108 @@
+<div class='container' id='project-div'>
+	<div class="card-deck">
+
+		<div class="card">
+		
+			<a href="http://jonathan-louis-com.stackstaging.com/twitter-clone/">
+				<img src="http://jonathan-louis-com.stackstaging.com/portfolio/twitter-clone.PNG" class="card-img-top" alt="...">
+			</a>
+			
+			<div class="card-body">
+				<a href="http://jonathan-louis-com.stackstaging.com/twitter-clone/">
+					<h5 class="card-title">Twitter Clone</h5>
+				</a>
+				<p class="card-text">This site is a twitter clone that allows an account to be created and then to post tweets from that account up to 140 characters.</p>
+				<p class="card-text"><small class="text-muted">Completed April 2020</small></p>
+			</div>
+		</div>
+
+		<div class="card">
+		
+			<a href="http://jonathan-louis-com.stackstaging.com/7-mysql/secret-diary-login.php">
+				<img src="http://jonathan-louis-com.stackstaging.com/portfolio/secret-diary.PNG" class="card-img-top" alt="...">
+			</a>
+			
+			<div class="card-body">
+				<a href="http://jonathan-louis-com.stackstaging.com/7-mysql/secret-diary-login.php/">
+					<h5 class="card-title">Secret Diary</h5>
+				</a>
+				<p class="card-text">This site allows the user to create an account and then save a personal journal.</p>
+				<p class="card-text"><small class="text-muted">Completed April 2020</small></p>
+			</div>
+		</div>
+		
+		<div class="card">
+		
+			<a href="http://jonathan-louis-com.stackstaging.com/8-apis/weather-api.php">
+				<img src="http://jonathan-louis-com.stackstaging.com/portfolio/weather-api.PNG" class="card-img-top" alt="...">
+			</a>
+			
+			<div class="card-body">
+				<a href="http://jonathan-louis-com.stackstaging.com/8-apis/weather-api.php">
+					<h5 class="card-title">Open-Weather API App</h5>
+				</a>
+				<p class="card-text">This site uses the API from openweathermap.org to get the current weather for a location.</p>
+				<p class="card-text"><small class="text-muted">Completed March 2020</small></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card-deck">
+	
+	<div class="card">
+	
+		<a href="http://jonathan-louis-com.stackstaging.com/6-php/weather-scraper.php">
+			<img src="http://jonathan-louis-com.stackstaging.com/portfolio/weather-scraper.PNG" class="card-img-top" alt="...">
+		</a>
+		
+		<div class="card-body">
+			<a href="http://jonathan-louis-com.stackstaging.com/6-php/weather-scraper.php">
+				<h5 class="card-title">Weather Scraper</h5>
+			</a>
+			<p class="card-text">This site scrapes the HTML code from weather-locations.com to get the current weather for a city.</p>
+			<p class="card-text"><small class="text-muted">Completed March 2020</small></p>
+		</div>
+	</div>
+	
+	<div class="card">
+	
+		<a href="http://jonathan-louis-com.stackstaging.com/mastermind.py">
+			<img src="http://jonathan-louis-com.stackstaging.com/portfolio/mastermind.PNG" class="card-img-top" alt="...">
+		</a>
+		
+		<div class="card-body">
+			<a href="http://jonathan-louis-com.stackstaging.com/mastermind.py">
+				<h5 class="card-title">Mastermind Game</h5>
+			</a>
+			<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+			<p class="card-text"><small class="text-muted">Completed April 2020</small></p>
+		</div>
+	</div>
+	
+	<div class="card">
+	<a href="http://jonathan-louis-com.stackstaging.com/thats-pretty-clever/">
+		<img src="http://jonathan-louis-com.stackstaging.com/portfolio/thats-pretty-clever.PNG" class="card-img-top" alt="...">
+	</a>
+		<div class="card-body">
+			<a href="http://jonathan-louis-com.stackstaging.com/thats-pretty-clever/">
+			<h5 class="card-title">That's Pretty Clever</h5>
+			</a>
+			<p class="card-text">Created the dice game That's Pretty Clever with javascript.</p>
+			<p class="card-text"><small class="text-muted">Completed April 2020</small></p>
+		</div>
+		</div>
+	</div>
+
+	<div class="card" style="width: 18rem;">
+		<a href="https://github.com/Jonathan-Louis">
+			<img src="http://jonathan-louis-com.stackstaging.com/portfolio/github.png" class="card-img-top" alt="...">
+		</a>
+		<div class="card-body">
+			<a href="https://github.com/Jonathan-Louis">
+				<h5 class="card-title">GitHub Projects</h5>
+			</a>
+			<p class="card-text">Here is the link to my github page that has source code from multiple projects.</p>
+		</div>
+	</div>
+	
+</div>
